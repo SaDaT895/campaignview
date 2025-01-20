2600be5cb993401a08a55204cc4bd3fa40fe691e
Sadat Ahmad <sadatahmadofficial@gmail.com>
2025-01-20 22:58:24 +0100

    feat(frontend): add form functionality

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,12 +2,15 @@ import { Component, inject } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { DataService } from '../data.service';
 import { MatButtonModule } from '@angular/material/button';
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { Campaign } from '../campaign';
 import {
-  MAT_DIALOG_DATA,
   MatDialog,
   MatDialogActions,
   MatDialogClose,
@@ -15,7 +18,8 @@ import {
   MatDialogRef,
   MatDialogTitle,
 } from '@angular/material/dialog';
-
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 @Component({
   selector: 'app-home',
   imports: [MatTableModule, MatButtonModule],
@@ -26,7 +30,12 @@ export class HomeComponent {
   dataService = inject(DataService);
   dialog = inject(MatDialog);
   addData() {
-    this.dialog.open(NewCampaignDialog);
+    const dialogRef = this.dialog.open(NewCampaignDialog);
+    dialogRef
+      .afterClosed()
+      .subscribe(
+        (campaign) => campaign && this.dataService.createCampaign(campaign)
+      );
   }
   campaigns$ = this.dataService.getCampaigns();
   displayedColumns = ['id', 'name', 'createdAt', 'endsAt'];
@@ -35,17 +44,30 @@ export class HomeComponent {
 @Component({
   selector: 'new-campaign-dialog',
   templateUrl: 'new-campaign-dialog.html',
+  styleUrl: './home.component.scss',
   imports: [
     MatFormFieldModule,
     MatInputModule,
-    FormsModule,
+    ReactiveFormsModule,
     MatButtonModule,
     MatDialogTitle,
     MatDialogContent,
     MatDialogActions,
     MatDialogClose,
+    MatDatepickerModule,
   ],
+  providers: [provideNativeDateAdapter()],
 })
 export class NewCampaignDialog {
   readonly dialogRef = inject(MatDialogRef<NewCampaignDialog>);
+  form = new FormGroup({
+    name: new FormControl('', { validators: [Validators.required] }),
+    startDate: new FormControl<Date>(new Date()),
+    endDate: new FormControl<Date | undefined>(undefined),
+    budgetAllocated: new FormControl<number>(0),
+  });
+
+  submit() {
+    this.dialogRef.close(this.form.value);
+  }
 }
